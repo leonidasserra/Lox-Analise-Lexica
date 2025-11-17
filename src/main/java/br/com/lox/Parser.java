7c49e6f898a0ee29e94c252a6b12038db8dfffef
Leonidas <leonidas.serra16@gmail.com>
2025-11-17 16:56:21 -0300

    feat(parser): adid lasse Parser e utilitários essenciais

---
+++ src/main/java/br/com/lox/Parser.java
@@ -0,0 +1,60 @@
+package br.com.lox;
+
+
+import java.util.List;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    // ============================================
+    // MÉTODOS BASE
+    // ============================================
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == TokenType.EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+        throw new RuntimeException(message + " Encontrado: " + peek().lexeme);
+    }
+
+
+    public Expr parse() {
+        return null;
+    }
+}
+
