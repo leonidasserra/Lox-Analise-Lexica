0f3df28d868080456d5024b37be1089acb184dc0
Isabela <isabela.moreira8721@gmail.com>
2025-11-23 18:02:06 -0300

    feat: adição da classe RuntimeError

--- src/main/java/br/com/lox/Lox.java
+++ src/main/java/br/com/lox/Lox.java
@@ -61,4 +61,11 @@ public class Lox {
         }
     }
 
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
+    static boolean hadRuntimeError = false;
+
 }
