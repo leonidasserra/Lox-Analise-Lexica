3c0b33a4e85eb1bfe4f002c46a7361a87d52736c
Isabela <isabela.moreira8721@gmail.com>
2025-12-02 15:20:39 -0300

    fix: adicionar suporte ao visitVariableExpr no AstPrinter

--- src/main/java/br/com/lox/AstPrinter.java
+++ src/main/java/br/com/lox/AstPrinter.java
@@ -27,9 +27,23 @@ public class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return parenthesize("assign " + expr.name.lexeme, expr.value);
+    }
+
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
-
         builder.append("(").append(name);
 
         for (Expr expr : exprs) {
--- src/main/java/br/com/lox/Main.java
+++ src/main/java/br/com/lox/Main.java
@@ -1,21 +1,47 @@
 package br.com.lox;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Uso: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            Lox.runFile(args[0]);
-        } else {
-            Lox.runPrompt();
-        }
+    public static void main(String[] args) {
+        String source = """
+                var a = 1;
+                var b = 2;
+
+                print a + b;
+
+                if (a < b) {
+                    print "a menor que b";
+                } else {
+                    print "a maior ou igual a b";
+                }
+
+                var i = 0;
+                while (i < 3) {
+                    print i;
+                    i = i + 1;
+                }
+
+                for (var j = 0; j < 3; j = j + 1) {
+                    print j * 10;
+                }
+                """;
+
+        // Executa o Lox
+        run(source);
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        Parser parser = new Parser(tokens);
+        List<Stmt> statements = parser.parse();
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret(statements);
     }
 }
